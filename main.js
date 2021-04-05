5ad95eab2a8e14e20a9476c42e0072a256c82d7d
Alessandro Candeago <candeago.alessandro@gmail.com>
2021-04-05 11:40:08 +0200

    Initial Commit

---
+++ main.js
@@ -0,0 +1,37 @@
+let isExConnected = false;
+let extension, address;
+
+
+async function connectWallet() {
+    const response = await connectToAndReturnWallet().catch((e) => console.error(e));
+    address =  response.address;
+    document.getElementById('connect-button').setAttribute('value', response.address);
+    document.getElementById('download').style.display = 'none';
+}
+
+
+async function connectToAndReturnWallet() {
+    if (!window.isTerraExtensionAvailable) {
+        await new Promise(resolve => setTimeout(resolve, 500));
+        if (!window.isTerraExtensionAvailable) {
+            throw new Error("Terra extension not installed or activated");
+        }
+    }
+
+    extension = new window.Terra.Extension();
+    return new Promise(async resolve => {
+        if (!isExConnected) {
+            isExConnected = true;
+            extension.on("onConnect", resolve);
+        }
+        return extension.connect();
+    });
+};
+
+async function generateQR(){
+    let addr = address || document.getElementById('address').value;
+    let formattedJSON = `{"name":"ledger","address":"${addr}","encrypted_key":"86064ac2525390c7f80f70baaade7c4d5429b4f5786eba8a366134334cd354f4GOe7YuZRzxyccvmGS+XlccuPzwE33I3XvqG5SNPAdQRTzAecPr77NhenT7PH03v4F50+YDZ0uFp9lEtOaMgqW15ihQOGwul4BpfY1+5AOmg="}`;
+    let encodedString = btoa(formattedJSON);
+    document.getElementById('img').style.display = 'block';
+    document.getElementById('img').src = 'https://api.qrserver.com/v1/create-qr-code/?color=fff&bgcolor=007ACC&size=250x250&data=' + encodeURIComponent('terrastation://wallet_recover/?payload=' + encodedString);
+}
